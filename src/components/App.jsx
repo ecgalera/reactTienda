62bae85f54f8e4d9cd992199c46cdca31ea3f0a5
Eduardo Galera <eduardocesargalera@gmail.com>
2024-05-20 19:20:22 -0300

    NavBar and Footer

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Button } from "react-bootstrap"
+import  Footer  from "./Footer"
+import Navbar from "./Navbar"
+
+function App() {
+ 
+  return (
+    <>
+      <Navbar/>
+     
+      <Footer/>
+    </>
+  )
+}
+
+export default App
